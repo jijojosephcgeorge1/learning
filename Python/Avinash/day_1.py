a189660e226ceebbbcdf651f80508935be4aff45
George, Jijo <Jijo.George@emc.com>
2025-03-22 18:25:20 +0530

    avinash_coaching v1

---
+++ Python/Avinash/day_1.py
@@ -0,0 +1,7 @@
+'''Topics learnt\
+1. Annotations: The description we give after each line of the code
+2. Memory: Heap and stack and how it is divided
+3. Refrence: How an object of any type is refrenced from stack to heap using hash key
+4. Usage of id()
+5. Usage of type()
+'''
---
+++ Python/Avinash/day_2.py
@@ -0,0 +1,10 @@
+'''
+1. Mutable and inmutable data types with example
+2. Nested data types having list in tuple and tuple in list.
+3. The concept of how object in view is considered when defining mutable and inmutable data types
+4. How all the default parameters are linked to object class internally.
+5. Basic usage of dir(), type(), isinstanceof() and help()
+6. How to read the o/p of help()
+7. How to use .join()
+8. Diffrent types of error statements (type error, divide by zero error...)
+'''
