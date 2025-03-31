e0dcbc25461ace024b048153a09f5d8f54328c3b
George, Jijo <Jijo.George@emc.com>
2025-03-31 18:55:41 +0530

    avinash_coaching v2

---
+++ Python/Avinash/day_3.py
@@ -0,0 +1,7 @@
+'''
+1. Difference b/w overloading and overriding
+2. Why is method overloading not supported in python (achieved using non assignment of variables and *args, **kwargs)
+3. String
+4. dunder functions eg: __init__ and __add__
+5. operator overloading
+'''
